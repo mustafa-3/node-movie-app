d8cdb96b32186af391f069c0083b55d5a46fae9a
Mustafa Sagun <101127558+mustafa-3@users.noreply.github.com>
2023-03-15 10:31:55 +0300

    fix

--- movie-app-client/src/App.js
+++ movie-app-client/src/App.js
@@ -25,7 +25,9 @@ const App = () => {
           name="Login"
           element={
             <PublicRouter>
-              <Login />
+              <MainLayout>
+                <Login />
+              </MainLayout>
             </PublicRouter>
           }
         />
